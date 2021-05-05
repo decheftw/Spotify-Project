d71d3e4fbe8faaceabee298f84c08b9b02ff7798
Gao Chen <devonc2000@gmail.com>
2021-05-05 11:49:33 -0400

    shows search results as grid. Nominate button affects data. Is disabled when movie is nominated or 5 noms reached

--- src/index.js
+++ src/index.js
@@ -5,58 +5,19 @@
 // change require to es6 import style
 import './style.scss';
 import {
-  BrowserRouter as Router, Route, NavLink, Switch,
+  BrowserRouter as Router,
 } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import Counter from './components/counter';
-import Controls from './components/controls';
 import reducers from './reducers';
 import Page from './components/front_page';
-
-const About = (props) => {
-  return <div> All there is to know about me </div>;
-};
-const Welcome = (props) => {
-  return <div>Welcome</div>;
-};
-
-const Test = (props) => {
-  return <div> ID: {props.match.params.id} </div>;
-};
-
-const FallBack = (props) => {
-  return <div>URL Not Found</div>;
-};
-
-const Nav = (props) => {
-  return (
-    <nav>
-      <ul>
-        <li><NavLink to="/" exact>Home</NavLink></li>
-        <li><NavLink to="/about" exact>About</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
-      </ul>
-    </nav>
-  );
-};
 
 const App = (props) => {
   return (
     <Router>
       <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/about" component={About} />
-          <Route exact path="/test/:id" component={Test} />
-          <Route component={FallBack} />
-        </Switch>
-        <Counter />
-        <Controls />
         <Page />
       </div>
     </Router>
